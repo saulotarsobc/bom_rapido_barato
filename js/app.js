d0a218d07988747be26668938d8b19c624eda2ed
Saulo Costa <saulotarsobc@gmail.com>
2023-03-17 14:39:32 -0300

    :frog:

--- js/app.js
+++ js/app.js
@@ -1,15 +1,13 @@
-const { bom, barato, rapido } = {
-    bom: document.getElementById('bom'),
-    barato: document.getElementById('barato'),
-    rapido: document.getElementById('rapido')
-};
+const bom = document.getElementById('bom'),
+    barato = document.getElementById('barato'),
+    rapido = document.getElementById('rapido');
 
-document.querySelectorAll('input').forEach(el => {
-    el.addEventListener('change', () => {
-        switch (el.id) {
+document.querySelectorAll('input').forEach(e => {
+    e.addEventListener('change', () => {
+        switch (e.dataset.sigla) {
             case "bom": if (bom.checked) if (rapido.checked && barato.checked) (Math.random() < 0.5) ? rapido.checked = false : barato.checked = false;
-            case "barato": if (barato.checked) if (rapido.checked && bom.checked) (Math.random() < 0.5) ? rapido.checked = false : bom.checked = false;
-            case "rapido": if (rapido.checked) if (bom.checked && barato.checked) (Math.random() < 0.5) ? bom.checked = false : barato.checked = false;
+            case "bar": if (barato.checked) if (rapido.checked && bom.checked) (Math.random() < 0.5) ? rapido.checked = false : bom.checked = false;
+            case "rap": if (rapido.checked) if (bom.checked && barato.checked) (Math.random() < 0.5) ? bom.checked = false : barato.checked = false;
         };
     });
 });
