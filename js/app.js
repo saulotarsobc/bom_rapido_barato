5644842896eb2252d1476f46ec921d815b1bb158
Saulo Costa <saulotarsobc@gmail.com>
2023-03-17 12:17:15 -0300

    :frog:

--- js/app.js
+++ js/app.js
@@ -1,40 +1,14 @@
-const inputs = document.querySelectorAll('input');
+const inputs = document.querySelectorAll('input'),
+    bom = document.getElementById('bom'),
+    barato = document.getElementById('barato'),
+    rapido = document.getElementById('rapido');
 
-const bom = document.getElementById('bom');
-const barato = document.getElementById('barato');
-const rapido = document.getElementById('rapido');
-
-inputs.forEach(input => {
-    input.addEventListener('click', e => calcular(input.id));
+inputs.forEach(el => {
+    el.addEventListener('change', () => {
+        switch (el.id) {
+            case "bom": if (bom.checked) if (rapido.checked && barato.checked) (Math.random() < 0.5) ? rapido.checked = false : barato.checked = false;
+            case "barato": if (barato.checked) if (rapido.checked && bom.checked) (Math.random() < 0.5) ? rapido.checked = false : bom.checked = false;
+            case "rapido": if (rapido.checked) if (bom.checked && barato.checked) (Math.random() < 0.5) ? bom.checked = false : barato.checked = false;
+        };
+    });
 });
-
-function calcular(id) {
-    console.log(id);
-    switch (id) {
-        case "bom": {
-            if (bom.checked) {
-                if (rapido.checked && barato.checked) lento();
-            }
-        };
-        case "rapido": { };
-        case "barato": {
-            if (barato.checked) {
-                if (rapido.checked && bom.checked) ruin();
-            }
-        };
-    };
-};
-
-
-/* cenarios */
-function ruin() {
-    bom.checked = false;
-}
-
-function caro() {
-    barato.checked = false;
-}
-
-function lento() {
-    rapido.checked = false;
-}
