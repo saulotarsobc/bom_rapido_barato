11b3038c8e452c69c915192245840d15d2f59025
Saulo Costa <saulotarsobc@gmail.com>
2023-03-17 15:45:56 -0300

    :frog:

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@ const a = document.getElementById('bom'),
     b = document.getElementById('barato'),
     c = document.getElementById('rapido');
 
-const desmarca = (p1, p2) => Math.random() > 0.5 ? p1.checked = false : p2.checked = false;
+const desmarca = (p1, p2) => Math.random() < 0.5 ? p1.checked = false : p2.checked = false;
 
 document.querySelectorAll('input').forEach(e => {
     e.addEventListener('click', () => {
         const { sig } = e.dataset;
-        if (sig == 'bom' && a.checked) if (b.checked && c.checked) desmarca(b, c)
-        if (sig == 'bar' && b.checked) if (a.checked && c.checked) desmarca(a, c);
-        if (sig == 'rap' && c.checked) if (b.checked && a.checked) desmarca(b, a);
+        (sig == 'bom' && a.checked) ? (b.checked && c.checked) ? desmarca(b, c) : "" : "";
+        (sig == 'bar' && b.checked) ? (a.checked && c.checked) ? desmarca(a, c) : "" : "";
+        (sig == 'rap' && c.checked) ? (b.checked && a.checked) ? desmarca(b, a) : "" : "";
     });
 });
