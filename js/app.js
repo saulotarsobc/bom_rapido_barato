e9938d0024be143969325fe9536ff4c4c2b03dea
Saulo Costa <saulotarsobc@gmail.com>
2023-03-17 15:39:17 -0300

    :frog:

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const a = document.getElementById('bom'),
     b = document.getElementById('barato'),
     c = document.getElementById('rapido');
 
-const desmarca = (p1, p2) => Math.random() < 0.5 ? p1.checked = false : p2.checked = false;
+const desmarca = (p1, p2) => Math.random() > 0.5 ? p1.checked = false : p2.checked = false;
 
 document.querySelectorAll('input').forEach(e => {
     e.addEventListener('click', () => {
