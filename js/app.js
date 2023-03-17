1531bd0c4e1e627e29fb0cc1b76ea8057b072441
Saulo Costa <saulotarsobc@gmail.com>
2023-03-17 15:22:02 -0300

    :frog:

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
-const bom = document.getElementById('bom'),
-    barato = document.getElementById('barato'),
-    rapido = document.getElementById('rapido');
+const a = document.getElementById('bom'),
+    b = document.getElementById('barato'),
+    c = document.getElementById('rapido');
+
+const desmarca = (p1, p2) => Math.random() < 0.5 ? p1.checked = false : p2.checked = false;
 
 document.querySelectorAll('input').forEach(e => {
-    e.addEventListener('change', () => {
-        switch (e.dataset.sigla) {
-            case "bom": if (bom.checked) if (rapido.checked && barato.checked) (Math.random() < 0.5) ? rapido.checked = false : barato.checked = false;
-            case "bar": if (barato.checked) if (rapido.checked && bom.checked) (Math.random() < 0.5) ? rapido.checked = false : bom.checked = false;
-            case "rap": if (rapido.checked) if (bom.checked && barato.checked) (Math.random() < 0.5) ? bom.checked = false : barato.checked = false;
-        };
+    e.addEventListener('click', () => {
+        const { sig } = e.dataset;
+        if (sig == 'bom' && a.checked) if (b.checked && c.checked) desmarca(b, c)
+        if (sig == 'bar' && b.checked) if (a.checked && c.checked) desmarca(a, c);
+        if (sig == 'rap' && c.checked) if (b.checked && a.checked) desmarca(b, a);
     });
 });
